58618a384db5fa24346b5378dcfdc4578d5e5ea8
Sumit Kushwaha <su.mit.6b@gmail.com>
2020-01-25 14:00:54 +0530

    revamped

---
+++ precache-manifest.2bbaf6b386cd3dda8ff0684bc5f895a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2563352695dd9ded0df1fbda57c8de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12dcdb29dc8d5931091e",
+    "url": "/static/css/main.41347242.chunk.css"
+  },
+  {
+    "revision": "5fb21fb2a50c86bdff5b",
+    "url": "/static/js/2.224b2f9d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.224b2f9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12dcdb29dc8d5931091e",
+    "url": "/static/js/main.4602a730.chunk.js"
+  },
+  {
+    "revision": "5cac8d751b59e2ed7ef7",
+    "url": "/static/js/runtime-main.651c1296.js"
+  }
+]);
